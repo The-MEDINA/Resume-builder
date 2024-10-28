9ef47cd0991402e19748d53e335fda71633a73a6
Dave <147880142+The-MEDINA@users.noreply.github.com>
2024-10-27 21:35:49 -0400

    oh my god learning new things is painful sometimes

--- ResumeBuilder/canvas.js
+++ ResumeBuilder/canvas.js
@@ -1,11 +1,9 @@
-console.log('hello from the console');
-
-const canvas = document.querySelector ('canvas')
+// setting up the canvas (I think)
+const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-context.fillRect(100, 100, 100, 100);
-
+console.log(canvas.width);
+console.log(canvas.height);
+//context.fillRect(0, 0, canvas.width, 100);
