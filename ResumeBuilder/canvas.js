ef57315e11350c77ef8dd6a9044e1b7ca3ad86b2
The-MEDINA <kidcandy24@gmail.com>
2024-10-31 23:35:28 -0400

    holy sh*t I did a lot today (How did i make it this far without node.js)
    I feel like a god cause I added p5.js and I made it work properly I'm f*cking CODING

--- ResumeBuilder/canvas.js
+++ ResumeBuilder/canvas.js
@@ -1,9 +1,16 @@
+import('./p5.js')
 // setting up the canvas (I think)
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
+function setup()
+{
+    var canvas = createCanvas(400, 400)
+    canvas.parent('fixp5js')
+}
+function draw()
+{
+    strokeWeight(3)
+    fill('#FF0000')
+    rect(0, 0, 100, 100)
+}
 //console.log(canvas.width);
 //console.log(canvas.height);
 //context.fillRect(0, 0, canvas.width, 100);
