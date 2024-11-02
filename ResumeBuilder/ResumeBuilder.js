02f9cb8564a77097a5a32d727df660ff2e9ad126
The-MEDINA <kidcandy24@gmail.com>
2024-11-02 15:42:51 -0400

    this goddamn button I swear why did it take so much effort to make a damn button
    I'm crying, p5js was supposed to help make this easier

--- ResumeBuilder/ResumeBuilder.js
+++ ResumeBuilder/ResumeBuilder.js
@@ -2,18 +2,26 @@
 import('./libraries/p5.js')
 let canvasWidth;
 let canvasHeight;
+let button;
 function setup()
 {
-    canvasWidth = windowWidth - 20;
+    canvasWidth = windowWidth/2;
     canvasHeight = windowHeight;
-    var canvas = createCanvas(windowWidth/2, windowHeight)
+    var canvas = createCanvas(windowWidth / 2, windowHeight)
+    button = createButton('click me')
     canvas.parent('fixp5js')
+    button.position(windowWidth/2 -50, 200+10)
+    button.mousePressed(testCode)  
 }
-function windowResized()
+/*function windowResized()
 {
     resizeCanvas(windowWidth/2, windowHeight);
-}
+}*/
 function draw()
 {
     background('#FAFAFA')
+}
+function testCode()
+{
+    alert('pls work')
 }
