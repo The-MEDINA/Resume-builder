ae603d447dec2c61cd2ccf9ba93a06ef56cc5046
the-MEDINA <kidcandy24@gmail.com>
2024-11-22 17:11:09 -0500

    I... what was I doing without Next.js oh my god :sob:

---
+++ resume-builder/src/app/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <div className="topnav">
+        <a>Oh my god why did it take so long to learn how to do this</a>
+    </div>
+      <p>So is this one hidden?</p>
+      <p>This too?</p>
+      <p>see me?</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+      <p>scroll please</p>
+    </div>
+  );
+}
