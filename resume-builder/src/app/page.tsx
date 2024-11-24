bdd1a4e3697a345e4aa3b4a1ee6c221ae4d446c7
the-MEDINA <kidcandy24@gmail.com>
2024-11-24 14:52:30 -0500

    it took... a little while.. to figure out how to import a script again

--- resume-builder/src/app/page.tsx
+++ resume-builder/src/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
-
+import Script from "next/script";
 export default function Home() {
   return (
     <div>
     <div className="topnav">
-        <a>Oh my god why did it take so long to learn how to do this</a>
+        <a>it was so easy to bring this over</a>
     </div>
       <p>So is this one hidden?</p>
       <p>This too?</p>
-      <p>see me?</p>
+      <p>visible</p>
+      <div id="app">
+        <Script src="editor.js"/>
+      </div>
       <p>scroll please</p>
       <p>scroll please</p>
       <p>scroll please</p>
