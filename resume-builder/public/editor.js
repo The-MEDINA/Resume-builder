0aded3c4b5a3cffa3f8455e1406d2bd0375c3401
the-MEDINA <kidcandy24@gmail.com>
2024-11-24 16:46:17 -0500

    I made a grid YAAY (i'm also worried about building this all out of HTML elements)

--- resume-builder/public/editor.js
+++ resume-builder/public/editor.js
@@ -3,9 +3,16 @@ const app = document.getElementById("app");
 // 2. Create a new <p></p> element programmatically
 const p = document.createElement("p");
 const q = document.createElement("p");
+const grid = document.createElement("div");
+
+// testing??
+grid.classList.add('test-grid');
+// it worked :>
+
 // 3. Add the text content
 p.textContent = "text element from editor.js here";
 q.textContent = "can I add another?";
 // 4. Append the p element to the div element
-app?.appendChild(p);
-app?.appendChild(q);
+grid.appendChild(p);
+grid.appendChild(q);
+app?.appendChild(grid);
