2c6b0563468df92c9e5e10aea2004d1b2be2f3aa
the-MEDINA <kidcandy24@gmail.com>
2024-11-29 15:04:43 -0500

    Cleaned it up a little bit, just as more prepwork

--- resume-builder/public/editor.js
+++ resume-builder/public/editor.js
@@ -1,30 +1,31 @@
-
+/// Setup of the editor here.
 const app = document.getElementById("app");
-
 const grid = document.createElement("div");
 const resume = document.createElement("div");
 const addElements = document.createElement("div");
 const tweakElements = document.createElement("div");
 const spacer = document.createElement("div");
 
-const p = document.createElement("p");
-const q = document.createElement("p");
-const r = document.createElement("p");
-
-grid.classList.add('test-grid');
+// Adding classes to elements created.
+grid.classList.add('editor-grid');
 addElements.classList.add('addElements');
 resume.classList.add('resume');
 tweakElements.classList.add('tweakElements');
 
+// Elements for testing.
+const p = document.createElement("p");
+const q = document.createElement("p");
+const r = document.createElement("p");
 p.textContent = "Add elements column";
 q.textContent = "Resume column";
 r.textContent = "Change elements column";
-// 4. Append the p element to the div element
+
+// Append elements to create the editor.
+app?.appendChild(grid);
+app?.appendChild(addElements);
 grid.appendChild(spacer);    
 grid.appendChild(resume);    
 grid.appendChild(tweakElements);   
 addElements.appendChild(p); 
 resume.appendChild(q);
 tweakElements.appendChild(r);
-app?.appendChild(grid);
-app?.appendChild(addElements);
