25b2344e353e5bac6d5cab747a7ded3ca95107f6
the-MEDINA <kidcandy24@gmail.com>
2024-12-13 16:47:38 -0500

    cleanup and preparation for the big tags and the removal of the old edit elements section

--- resume-builder/public/editor.js
+++ resume-builder/public/editor.js
@@ -4,6 +4,7 @@ const grid = document.createElement("div");
 const resume = document.createElement("div");
 const addElements = document.createElement("div");
 const tweakElements = document.createElement("div");
+const skillTags = document.createElement("div");
 const spacer = document.createElement("div");
 let listOfElements = [];
 let ElementEdits = [];
@@ -12,25 +13,30 @@ let ElementEdits = [];
 grid.classList.add('editor-grid');
 addElements.classList.add('addElements');
 resume.classList.add('resume');
+skillTags.classList.add('skills');
 tweakElements.classList.add('tweakElements');
 
 // Elements for testing.
 const p = document.createElement("p");
 const q = document.createElement("p");
 const r = document.createElement("p");
+const s = document.createElement("p");
 p.textContent = "Add elements column (Here you select elements to add)";
 q.textContent = "Resume column (goal is to click on each element to edit)";
 r.textContent = "Change elements column (edit everything about the element on the side)";
+s.textContent = "skills column (where the fun part is, the tags :>)";
 
 // Append elements to create the editor.
 app?.appendChild(grid);
 app?.appendChild(addElements);
 grid.appendChild(spacer);    
 grid.appendChild(resume);    
+grid.appendChild(skillTags);
 grid.appendChild(tweakElements);   
 addElements.appendChild(p); 
 resume.appendChild(q);
 tweakElements.appendChild(r);
+skillTags.appendChild(s);
 
 // Add title button
 const addTitleButton = document.createElement("button");
@@ -51,10 +57,7 @@ function AddTitle()
     listOfElements.push(document.createElement("div"));
     listOfElements[listOfElements.length-1].classList.add('Title');
     listOfElements[listOfElements.length-1].appendChild(CreateTitle());
-    listOfElements[listOfElements.length-1].setAttribute("index",[ElementEdits.length]);
-    ElementEdits.push(document.createElement("div"));
-    ElementEdits[ElementEdits.length-1].appendChild(CreateEditButton(ElementEdits.length-1));
-    ElementEdits[ElementEdits.length-1].setAttribute("index",[ElementEdits.length-1]);
+    listOfElements[listOfElements.length-1].setAttribute("index",[listOfElements.length-1]);
     DrawElements();
 }
 
@@ -66,10 +69,7 @@ function AddElement()
     listOfElements[listOfElements.length-1].appendChild(CreateHeader());
     listOfElements[listOfElements.length-1].appendChild(CreateDate());
     listOfElements[listOfElements.length-1].appendChild(CreateDescription());
-    listOfElements[listOfElements.length-1].setAttribute("index",[ElementEdits.length]);
-    ElementEdits.push(document.createElement("div"));
-    ElementEdits[ElementEdits.length-1].appendChild(CreateEditButton(ElementEdits.length-1));
-    ElementEdits[ElementEdits.length-1].setAttribute("index",[ElementEdits.length-1]);
+    listOfElements[listOfElements.length-1].setAttribute("index",[listOfElements.length-1]);
     DrawElements();
 }
 
@@ -79,7 +79,6 @@ function DrawElements()
     for (let i = 0; i < listOfElements.length; i++)
     {
         resume.appendChild(listOfElements[i]);
-        tweakElements.appendChild(ElementEdits[i]);
     }
 }
 
@@ -90,7 +89,6 @@ function CreateEditButton(_index)
     if (listOfElements[_index].classList.contains("Title"))
     {
         thisButton.textContent = "Edit title " + (_index) + " button";
-        ElementEdits[_index].setAttribute("index",[_index]);
         thisButton.addEventListener('click', function() {TitleDropDownMenu(thisButton.parentElement.getAttribute("index"))});
     }
     else
@@ -107,7 +105,7 @@ function CreateTitle()
     const thisTitle = document.createElement("button");
     thisTitle.setAttribute("id","title");
     thisTitle.addEventListener('click', function() {ChangeText(thisTitle)});
-    thisTitle.textContent = "Title " + (ElementEdits.length);
+    thisTitle.textContent = "Title " + (listOfElements.length-1);
     titleDiv.appendChild(thisTitle);
     return titleDiv;
 }
@@ -118,7 +116,7 @@ function CreateHeader()
     const elementHeader = document.createElement("button");
     elementHeader.setAttribute("id","elementHeader");
     elementHeader.addEventListener('click', function() {ChangeText(elementHeader)});
-    elementHeader.textContent = "Job title or something - location |E" + (ElementEdits.length);
+    elementHeader.textContent = "Job title or something - location |E" + (listOfElements.length-1);
     elementHeaderDiv.appendChild(elementHeader);
     return elementHeaderDiv;
 }
@@ -130,7 +128,7 @@ function CreateDate()
     const elementDate = document.createElement("button");
     elementDate.setAttribute("id","elementDate");
     elementDate.addEventListener('click', function() {ChangeText(elementDate)});
-    elementDate.textContent = "DateStart - DateEnd |E" + (ElementEdits.length);
+    elementDate.textContent = "DateStart - DateEnd |E" + (listOfElements.length-1);
     elementDateDiv.appendChild(elementDate);
     return elementDateDiv;
 }
@@ -142,34 +140,9 @@ function CreateDescription()
     const elementDesc = document.createElement("button");
     elementDesc.setAttribute("id","elementDesc");
     elementDesc.addEventListener('click', function() {ChangeText(elementDesc)});
-    elementDesc.textContent = "Description of something |E" + (ElementEdits.length);
+    elementDesc.textContent = "Description of something |E" + (listOfElements.length-1);
     createDescDiv.appendChild(elementDesc);
     return createDescDiv;
-}
-
-// adds the dropdown menu for title edit buttons.
-function TitleDropDownMenu(elementIndex)
-{
-    let intIndex = +elementIndex; // oh my god... this... this is NASTY
-    while (ElementEdits[intIndex].firstChild) 
-    {
-        ElementEdits[intIndex].removeChild(ElementEdits[intIndex].lastChild);
-    }
-    // Delete Button.
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = '|Delete Title ' + intIndex + '|';
-    deleteButton.addEventListener('click', function() {DeleteSomething(intIndex)});
-    ElementEdits[intIndex].appendChild(deleteButton);
-    // Update text Button.
-    const TextButton = document.createElement("button");
-    TextButton.textContent = '|Title text @ ' + intIndex + '|';
-    TextButton.addEventListener('click', function() {ChangeTitleText(intIndex)});
-    ElementEdits[intIndex].appendChild(TextButton);
-    // Back Button.
-    const backButton = document.createElement("button");
-    backButton.textContent = '|Back|';
-    backButton.addEventListener('click', function() {AdjustElements(intIndex)});
-    ElementEdits[intIndex].appendChild(backButton);
 }
 
 // Deletes something from the resume according to its index attribute.
@@ -183,7 +156,6 @@ function DeleteSomething(_index)
         removeThese.remove();        
     }
     listOfElements.splice(_index,1);   
-    ElementEdits.splice(_index,1);    
     AdjustElements();
     DrawElements();
 }
@@ -222,14 +194,13 @@ function ChangeText(element)
 }
 
 // Goes with ChangeText. Finishes up changing text.
-// I moved this to another method thinking it would make the code look nicer. I guess it did, but.. man, this method is NOT needed
 function FinishChangeText(scannerDivToRemove, elementToChange, valueToChangeWith)
 {
     if (valueToChangeWith == null || valueToChangeWith == "")
     {
         elementToChange.parentNode.remove();
         // this is such a bad fix... but it *does* work. The plan is to rewrite this for loop so that this isn't so nasty.
-        for (let i = 0; i < ElementEdits.length; i++){
+        for (let i = 0; i < listOfElements.length; i++){
             if (listOfElements[i].children.length == 0){
                 DeleteSomething(i);
                 i -= 1;
@@ -243,22 +214,8 @@ function FinishChangeText(scannerDivToRemove, elementToChange, valueToChangeWith
 // Adjusts the indeces and messes with the buttons to prevent any bugs and wacky situations.
 function AdjustElements()
 {
-    for (let i = 0; i < ElementEdits.length; i++)
+    for (let i = 0; i < listOfElements.length; i++)
     {
         listOfElements[i].setAttribute("index",i);
-        ElementEdits[i].setAttribute("index",i);   
     }
-    for (let i = 0; i < ElementEdits.length; i++)
-        {
-            while (ElementEdits[i].firstChild) 
-            {
-                ElementEdits[i].removeChild(ElementEdits[i].lastChild);
-            }
-            ElementEdits[i].appendChild(CreateEditButton(i));
-        }
 }
-
-// TODO:
-// change it so that if an element child or whatever its called has nothing in it, remove the div that contains it.
-// If an element has nothing in it, delete it.
-// make sure when they're deleted, the indeces and list are adjusted.
