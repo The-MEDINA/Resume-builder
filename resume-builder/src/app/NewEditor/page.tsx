9e53e814de869e83599226c75076b9705f64c696
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2025-02-12 21:11:39 -0500

    oh my god that's a lot of progress
    things can be added and skills are a fourth of the way done to being implemented

--- resume-builder/src/app/NewEditor/page.tsx
+++ resume-builder/src/app/NewEditor/page.tsx
@@ -1,13 +1,19 @@
 'use client'
-import { Skill } from "@/app/Skills/page";
-import { split } from "postcss/lib/list";
+import { ArrayToSkillType, Skill } from "@/app/Skills/page";
+import { GetSavedSkillList } from "../../../public/HelperScripts/skillTags";
 let resume: any = [];
+let listOfSkills: Skill[] = ArrayToSkillType(GetSavedSkillList());
 export default function NewEditor() {
   document.onreadystatechange = function () {
     if (document.readyState == "complete") 
     {
       document.getElementById("app")?.classList.add('editor-grid');
-      document.getElementById("addRawTitle")?.addEventListener('click', function() {AddTitle()});
+      document.getElementById("addRawSubtitle")?.addEventListener('click', function() {AddRawElement("Subtitle")});
+      document.getElementById("addRawDate")?.addEventListener('click', function() {AddRawElement("Date")});
+      document.getElementById("addRawDesc")?.addEventListener('click', function() {AddRawElement("Description")});
+      document.getElementById("addRawTitle")?.addEventListener('click', function() {AddRawElement("Title")});
+      document.getElementById("addExperience")?.addEventListener('click', function() {AddElementGroup("Experience")});
+      console.log(listOfSkills);
       // something something look for and load a previously saved resume somewhere
       DisplayResume();
     }
@@ -53,8 +59,11 @@ function AddCSSFromString(HTMLElement: any, rawString: string)
   {
     case ("display"): { HTMLElement.style.display = splitString[1]; break; }
     case ("justify-content"): { HTMLElement.style.justifyContent = splitString[1]; break; }
+    case ("font-size"): { HTMLElement.style.fontSize = splitString[1]; break; }
+    case ("border"): { HTMLElement.style.border = splitString[1]; break; }
   }
 }
+
 // puts the resume onto the website.
 function DisplayResume()
 {
@@ -69,70 +78,271 @@ function DisplayResume()
   }
 }
 
-function AddTitle()
+function AddRawElement(elementName: string)
 {
-  console.log("AddTitle()");
-  let newTitle = new Title();
-  resume.push(newTitle);
-  DisplayResume();
+  switch (elementName)
+  {
+    case ("Title"): 
+    {
+      let newTitle = new Title();
+      resume.push(newTitle);
+      DisplayResume();
+      break;
+    }
+    case ("Subtitle"): 
+    {
+      let newSubtitle = new Subtitle();
+      resume.push(newSubtitle);
+      DisplayResume();
+      break;
+    }
+    case ("Description"): 
+    {
+      let newDesc = new Description();
+      resume.push(newDesc);
+      DisplayResume();
+      break;
+    }
+    case ("Date"): 
+    {
+      let newDate = new Date();
+      resume.push(newDate);
+      DisplayResume();
+      break;
+    }
+    default:
+      {
+        throw new Error("AddRawElement received a string that is not any basic resume element.");
+      }
+  }
 }
-/// The base that all resume elements draw from.
+
+// puts element groups, like experience, into the resume.
+function AddElementGroup(elementName: string)
+{
+  switch (elementName)
+  {
+    case ("Experience"): 
+    {
+      let newExp = new Experience();
+      let newTitle = new Subtitle();
+      let newDate = new Date();
+      let newDesc = new Description();
+      newExp.titles.push(newTitle);
+      newExp.dates.push(newDate);
+      newExp.desc.push(newDesc);
+      resume.push(newExp);
+      DisplayResume();
+      break;
+    }
+  }
+}
+
+// Creates a dropdown menu of a specific set of skills.
+// As much as I hate my previous implementation of this, the general idea is the only way I can think of.
+function SkillDropDownMenu(parent: string, parentToAppendTo: any)
+{
+  console.log("SkillDropDownMenu(string)");
+  let parentdiv = document.createElement("div");
+  for (let i = 0; i < listOfSkills.length; i++)
+  {
+    parentdiv.classList.add("skillDropDown");
+    if (listOfSkills[i].parent == parent)
+    {
+      let skillHolder = document.createElement("div");
+      let skill = document.createElement("button");
+      skill.textContent = listOfSkills[i].name;
+      // something about adding an event somewhere in here.
+      skill.addEventListener('mouseover', function() {SkillDropDownMenu(listOfSkills[i].name, skillHolder)});
+      skillHolder.appendChild(skill);
+      parentdiv.appendChild(skillHolder);
+      parentToAppendTo.appendChild(parentdiv);
+    }
+  }
+}
+
+/// The base that all of the fundamental resume elements draw from.
 interface ResumeElement {
   text: string;
-  textSize: number;
   cssOptions: string[];
+
+  // directly adds the element to the page.
   Display: (any);
+
+  // returns itself as an HTML element.
+  ConvertToHTML: (any);
 }
 
 // Title class.
 class Title implements ResumeElement {
   text: string;
-  textSize: number;
   cssOptions: string[];
   public constructor()
   {
-    this.text = "new title";
-    this.textSize = 48;
-    this.cssOptions = ["display: flex","justify-content: center"];
+    this.text = "New Title";
+    this.cssOptions = ["display: flex","justify-content: center","font-size: 48px"];
   }
 
   Display()
   {
     let displayText = document.createElement("p");
     displayText.textContent = this.text;
-    displayText.style.fontSize = (this.textSize + "px");
     for (let i = 0; i < this.cssOptions.length; i++)
     {
       AddCSSFromString(displayText, this.cssOptions[i]);
     }
     document.getElementById("Resume")?.appendChild(displayText);
   }
+
+  ConvertToHTML()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    return displayText;
+  }
 }
 
 // Description class, Used for long text. Medium size.
-class Description {
+class Description implements ResumeElement {
   text: string;
+  cssOptions: string[];
   public constructor()
   {
-    this.text = "new description";
+    this.text = "New description that says a lot of words about something.";
+    this.cssOptions = ["font-size: 16px"];
+  }
+
+  Display()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    document.getElementById("Resume")?.appendChild(displayText);
+  }
+
+  ConvertToHTML()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    return displayText;
   }
 }
 
 // Date class. Smallest size.
-class Date {
+class Date implements ResumeElement {
   text: string;
+  cssOptions: string[];
   public constructor()
   {
-    this.text = "new date";
+    this.text = "Date start - Date end";
+    this.cssOptions = ["font-size: 12px"];
+  }
+
+  Display()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    document.getElementById("Resume")?.appendChild(displayText);
+  }
+
+  ConvertToHTML()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    return displayText;
   }
 }
 
 // Subtitle class, used for the title of things like job experiences. Large size.
-class Subtitle {
+class Subtitle implements ResumeElement{
   text: string;
+  cssOptions: string[];
   public constructor()
   {
-    this.text = "new subtitle";
+    this.text = "New Job title";
+    this.cssOptions = ["font-size: 24px"];
+  }
+
+  Display()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    document.getElementById("Resume")?.appendChild(displayText);
+  }
+
+  ConvertToHTML()
+  {
+    let displayText = document.createElement("p");
+    displayText.textContent = this.text;
+    for (let i = 0; i < this.cssOptions.length; i++)
+    {
+      AddCSSFromString(displayText, this.cssOptions[i]);
+    }
+    return displayText;
+  }
+}
+
+// skills class.
+// I literally only need this because I didn't realize types are just converted to strings or something at runtime.
+class Skills{
+  name: string;
+  parent: string;
+  address: string;
+  cssOptions: string[];
+  public constructor(skillType: Skill)
+  {
+    this.name = skillType.name;    
+    this.parent = skillType.parent;    
+    this.address = skillType.address;  
+    this.cssOptions = ["display: inline-block", "border: solid"]; 
+  }
+}
+// skillsBox class.
+// We'll see if this is necessary or not later.
+class SkillsBox {
+  skills: Skills[];
+  public constructor()
+  {
+    this.skills = [];
+  }
+
+  Display()
+  {
+
+  }
+
+  ConvertToHTML()
+  {
+    let parent = document.createElement("div");
+    // something about appending skills here.
+    let addButton = document.createElement("button");
+    addButton.textContent = "|Add new skill|";
+    addButton.addEventListener('mouseover', function() {SkillDropDownMenu("", parent)});
+    parent.appendChild(addButton)
+    return parent;
   }
 }
 
@@ -141,12 +351,33 @@ class Experience {
   titles: Subtitle[];
   dates: Date[];
   desc: Description[];
-  skills: Skill[];
+  skillsBox: SkillsBox;
+  cssOptions: string[];
   public constructor()
   {
     this.titles = [];
     this.dates = [];
     this.desc = [];
-    this.skills = [];
+    this.skillsBox = new SkillsBox();
+    this.cssOptions = [];
+  }
+
+  Display()
+  {
+    let parent = document.createElement("div");
+    for (let i = 0; i < this.titles.length; i++)
+    {
+      parent.appendChild(this.titles[i].ConvertToHTML());
+    }
+    for (let i = 0; i < this.dates.length; i++)
+    {
+      parent.appendChild(this.dates[i].ConvertToHTML());
+    }
+    for (let i = 0; i < this.desc.length; i++)
+    {
+      parent.appendChild(this.desc[i].ConvertToHTML());
+    }
+    parent.appendChild(this.skillsBox.ConvertToHTML());
+    document.getElementById("Resume")?.appendChild(parent);
   }
 }
